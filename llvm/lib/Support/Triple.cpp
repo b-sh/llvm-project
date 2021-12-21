f0e4dac610ce0d1a692efd0200b851a530e508da
b-sh <b-sh@gmx.net>
2021-12-21 22:01:38 +0100

    Add TriCore Target
    This is based on
    - https://github.com/TriDis/llvm-tricore
    - sha: 4bfc5ee073becdcf799978431a9c03045b1091a2
    - llvm version 3.7
    
    Just integrating into new release 13

--- llvm/lib/Support/Triple.cpp
+++ llvm/lib/Support/Triple.cpp
@@ -78,6 +78,7 @@ StringRef Triple::getArchTypeName(ArchType Kind) {
   case x86:            return "i386";
   case x86_64:         return "x86_64";
   case xcore:          return "xcore";
+  case tricore:        return "tricore";
   }
 
   llvm_unreachable("Invalid ArchType!");
@@ -158,6 +159,8 @@ StringRef Triple::getArchTypePrefix(ArchType Kind) {
 
   case ve:          return "ve";
   case csky:        return "csky";
+
+  case tricore:        return "tricore";
   }
 }
 
@@ -332,6 +335,7 @@ Triple::ArchType Triple::getArchTypeForLLVMName(StringRef Name) {
     .Case("renderscript64", renderscript64)
     .Case("ve", ve)
     .Case("csky", csky)
+    .Case("tricore", tricore)
     .Default(UnknownArch);
 }
 
@@ -465,6 +469,7 @@ static Triple::ArchType parseArch(StringRef ArchName) {
     .Case("wasm32", Triple::wasm32)
     .Case("wasm64", Triple::wasm64)
     .Case("csky", Triple::csky)
+    .Case("tricore", Triple::tricore)
     .Default(Triple::UnknownArch);
 
   // Some architectures require special parsing logic just to compute the
@@ -737,6 +742,7 @@ static Triple::ObjectFormatType getDefaultFormat(const Triple &T) {
   case Triple::thumbeb:
   case Triple::ve:
   case Triple::xcore:
+  case Triple::tricore:
     return Triple::ELF;
 
   case Triple::ppc64:
@@ -1305,6 +1311,7 @@ static unsigned getArchPointerBitWidth(llvm::Triple::ArchType Arch) {
   case llvm::Triple::wasm32:
   case llvm::Triple::x86:
   case llvm::Triple::xcore:
+  case llvm::Triple::tricore:
     return 32;
 
   case llvm::Triple::aarch64:
@@ -1390,6 +1397,7 @@ Triple Triple::get32BitArchVariant() const {
   case Triple::wasm32:
   case Triple::x86:
   case Triple::xcore:
+  case Triple::tricore:
     // Already 32-bit.
     break;
 
@@ -1431,6 +1439,7 @@ Triple Triple::get64BitArchVariant() const {
   case Triple::tce:
   case Triple::tcele:
   case Triple::xcore:
+  case Triple::tricore:
     T.setArch(UnknownArch);
     break;
 
@@ -1612,6 +1621,7 @@ bool Triple::isLittleEndian() const {
   case Triple::x86:
   case Triple::x86_64:
   case Triple::xcore:
+  case Triple::tricore:
     return true;
   default:
     return false;
